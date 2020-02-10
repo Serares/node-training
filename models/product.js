d0ea9a97a8553f8f242e33b7f3f8eb4f0433c6a7
Serares <jianu.rares30@gmail.com>
2020-02-10 19:20:50 +0200

    model for the product

--- models/product.js
+++ models/product.js
@@ -1,15 +1,17 @@
-const products = [];
+const fs = require('fs');
+const path = require('path');
 
-module.exports = class Product{
-    constructor(title){
+module.exports = class Product {
+    constructor(title) {
         this.title = title;
     }
 
-    save(){
-        products.push(this);
+    save() {
+        // create a data folder in the root folder and store a json file
+        const p = path.join(path.dirname(process.mainModule.filename), 'data', 'products.json');
     }
 
-    static fetchAll(){
+    static fetchAll() {
         // static so it can be called directly on the class 
         return products;
     }
